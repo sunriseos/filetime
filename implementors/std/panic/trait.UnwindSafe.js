c684a98165ff5338d0d3d967a0168308557c859a
ci <ci>
2019-09-06 20:22:28 +0000

    init

---
+++ implementors/std/panic/trait.UnwindSafe.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["filetime"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/panic/trait.UnwindSafe.html\" title=\"trait std::panic::UnwindSafe\">UnwindSafe</a> for <a class=\"struct\" href=\"filetime/struct.FileTime.html\" title=\"struct filetime::FileTime\">FileTime</a>",synthetic:true,types:["filetime::FileTime"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
